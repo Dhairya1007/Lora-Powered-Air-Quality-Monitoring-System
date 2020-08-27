784511ffedd231307015c09e48b97d88090f308a
Dhairya Parikh <dhairyaparikh1998@gmail.com>
2020-08-27 21:12:10 +0530

    Create Final Node Code.ino

---
+++ Final Node Code.ino
@@ -0,0 +1,302 @@
+/*
+ * Project - Lora powered Air Quality Monitoring System
+ * Developer - Dhairya Parikh
+ * Company - Whizkey OPC
+ * Description - This is the final code for the Lora node.
+ */
+
+//Note: This code demonstrates the use of the Sharp Dust sensor instead of DSM501B but its demo code is provided in the git repo. 
+
+//-------- Imported Libraries ------------
+#include <lmic.h>
+#include <hal/hal.h>
+#include <SPI.h>
+#include <Wire.h>
+#include "SparkFunCCS811.h"
+
+//---------- Objects Initialization ------
+#define USE_AVG
+#define CCS811_ADDR 0x5A
+CCS811 mySensor(CCS811_ADDR);
+
+//------- Variable Declarations -----------
+int co2,tvoc;
+int pin2 = 8;
+unsigned long duration2;
+
+const int sharpLEDPin = 7;   // Arduino digital pin 7 connect to sensor LED.
+const int sharpVoPin = A1;   // Arduino analog pin 5 connect to sensor Vo.
+
+#ifdef USE_AVG
+#define N 100
+static unsigned long VoRawTotal = 0;
+static int VoRawCount = 0;
+#endif // USE_AVG
+
+// Set the typical output voltage in Volts when there is zero dust. 
+static float Voc = 0.6;
+
+// Use the typical sensitivity in units of V per 100ug/m3.
+const float K = 0.5;
+
+// LoRaWAN NwkSKey, network session key
+// This is the default Semtech key, which is used by the prototype TTN
+// network initially.
+//ttn
+static const PROGMEM u1_t NWKSKEY[16] = { 0xE6, 0x54, 0x89, 0xD6, 0x05, 0x57, 0x45, 0x99, 0xAB, 0x18, 0x25, 0x14, 0x34, 0xA1, 0x99, 0x30 };
+
+// LoRaWAN AppSKey, application session key
+// This is the default Semtech key, which is used by the early prototype TTN
+// network.
+static const u1_t PROGMEM APPSKEY[16] = { 0x7A, 0x12, 0xB2, 0x84, 0xCA, 0xED, 0xE2, 0xAD, 0x07, 0xA9, 0x4A, 0xDB, 0xA0, 0x55, 0x05, 0x5D };
+
+// LoRaWAN end-device address (DevAddr)
+static const u4_t DEVADDR = 0x26011C08; // <-- Change this address for every node!
+byte payload[8];
+
+// These callbacks are only used in over-the-air activation, so they are
+// left empty here (we cannot leave them out completely unless
+// DISABLE_JOIN is set in config.h, otherwise the linker will complain).
+void os_getArtEui (u1_t* buf) { }
+void os_getDevEui (u1_t* buf) { }
+void os_getDevKey (u1_t* buf) { }
+
+
+static uint8_t mydata[] = {0x00, 0x00};
+static osjob_t initjob,sendjob,blinkjob;
+
+// Schedule TX every this many seconds (might become longer due to duty
+// cycle limitations).
+const unsigned TX_INTERVAL = 5;
+
+// Pin mapping
+const lmic_pinmap lmic_pins = {
+    .nss = 10,
+    .rxtx = LMIC_UNUSED_PIN,
+    .rst = 9,
+    .dio = {2, 6, 7},
+};
+void do_send(osjob_t* j){
+    // Check if there is not a current TX/RX job running
+    if (LMIC.opmode & OP_TXRXPEND) {
+        Serial.println("OP_TXRXPEND, not sending");
+    } else {
+        // Prepare upstream data transmission at the next possible time.
+         LMIC_setTxData2(2, payload, sizeof(payload), 0);
+         Serial.println(mySensor.getCO2());
+         Serial.println(mySensor.getTVOC());
+    
+        Serial.println("Packet queued");
+        Serial.println(LMIC.freq);
+    }
+    // Next TX is scheduled after TX_COMPLETE event.
+}
+
+void onEvent (ev_t ev) {
+    Serial.print(os_getTime());
+    Serial.print(": ");
+    Serial.println(ev);
+    switch(ev) {
+        case EV_SCAN_TIMEOUT:
+            Serial.println("EV_SCAN_TIMEOUT");
+            break;
+        case EV_BEACON_FOUND:
+            Serial.println("EV_BEACON_FOUND");
+            break;
+        case EV_BEACON_MISSED:
+            Serial.println("EV_BEACON_MISSED");
+            break;
+        case EV_BEACON_TRACKED:
+            Serial.println("EV_BEACON_TRACKED");
+            break;
+        case EV_JOINING:
+            Serial.println("EV_JOINING");
+            break;
+        case EV_JOINED:
+            Serial.println("EV_JOINED");
+            break;
+        case EV_RFU1:
+            Serial.println("EV_RFU1");
+            break;
+        case EV_JOIN_FAILED:
+            Serial.println("EV_JOIN_FAILED");
+            break;
+        case EV_REJOIN_FAILED:
+            Serial.println("EV_REJOIN_FAILED");
+            break;
+        case EV_TXCOMPLETE:
+            Serial.println("EV_TXCOMPLETE (includes waiting for RX windows)");
+            if(LMIC.dataLen) {
+                // data received in rx slot after tx
+                Serial.print("Data Received: ");
+                Serial.write(LMIC.frame+LMIC.dataBeg, LMIC.dataLen);
+                Serial.println();
+            }
+            // Schedule next transmission
+            os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
+            break;
+        case EV_LOST_TSYNC:
+            Serial.println("EV_LOST_TSYNC");
+            break;
+        case EV_RESET:
+            Serial.println("EV_RESET");
+            break;
+        case EV_RXCOMPLETE:
+            // data received in ping slot
+            Serial.println("EV_RXCOMPLETE");
+            break;
+        case EV_LINK_DEAD:
+            Serial.println("EV_LINK_DEAD");
+            break;
+        case EV_LINK_ALIVE:
+            Serial.println("EV_LINK_ALIVE");
+            break;
+         default:
+            Serial.println("Unknown event");
+            break;
+    }
+}
+
+// Helper functions to print a data value to the serial monitor.
+void printValue(String text, unsigned int value, bool isLast = false) {
+  Serial.print(text);
+  Serial.print("=");
+  Serial.print(value);
+  if (!isLast) {
+    Serial.print(", ");
+  }
+}
+void printFValue(String text, float value, String units, bool isLast = false) {
+  Serial.print(text);
+  Serial.print("=");
+  Serial.print(value);
+  Serial.print(units);
+  if (!isLast) {
+    Serial.print(", ");
+  }
+}
+
+void setup() 
+{
+    Serial.begin(115200);
+    pinMode(sharpLEDPin, OUTPUT);
+    pinMode(pin2,INPUT);
+    while(!Serial);
+    Wire.begin(); //Inialize I2C Hardware
+  
+  if (mySensor.begin() == false)
+  {
+    Serial.print("CCS811 error. Please check wiring. Freezing...");
+    while (1);
+  }
+    
+    Serial.println("Starting");
+    #ifdef VCC_ENABLE
+    // For Pinoccio Scout boards
+    pinMode(VCC_ENABLE, OUTPUT);
+    digitalWrite(VCC_ENABLE, HIGH);
+    delay(1000);
+    #endif
+    
+    
+    // LMIC init
+    os_init();
+    // Reset the MAC state. Session and pending data transfers will be discarded.
+    LMIC_reset();
+    //LMIC_setClockError(MAX_CLOCK_ERROR * 1/100);
+    // Set static session parameters. Instead of dynamically establishing a session
+    // by joining the network, precomputed session parameters are be provided.
+    #ifdef PROGMEM
+    
+    uint8_t appskey[sizeof(APPSKEY)];
+    uint8_t nwkskey[sizeof(NWKSKEY)];
+    memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
+    memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
+    LMIC_setSession (0x1, DEVADDR, nwkskey, appskey);
+    #else
+    // If not running an AVR with PROGMEM, just use the arrays directly 
+    LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
+    #endif
+    
+    // Disable link check validation
+    LMIC_setLinkCheckMode(0);
+
+    // TTN uses SF9 for its RX2 window.
+    LMIC.dn2Dr = DR_SF9;
+    
+    // Set data rate and transmit power (note: txpow seems to be ignored by the library)
+    LMIC_setDrTxpow(DR_SF7,14);
+
+    // Start job
+    do_send(&sendjob);
+
+}
+
+void loop() 
+{
+    mySensor.readAlgorithmResults();
+
+    uint32_t co2 = mySensor.getCO2();
+    uint32_t tvoc = mySensor.getTVOC();
+    uint32_t reading = analogRead(A0);
+
+    digitalWrite(sharpLEDPin, LOW);
+
+  // Wait 0.28ms before taking a reading of the output voltage as per spec.
+  delayMicroseconds(280);
+
+  // Record the output voltage. This operation takes around 100 microseconds.
+  int VoRaw = analogRead(sharpVoPin);
+  
+  // Turn the dust sensor LED off by setting digital pin HIGH.
+  digitalWrite(sharpLEDPin, HIGH);
+
+  // Wait for remainder of the 10ms cycle = 10000 - 280 - 100 microseconds.
+  delayMicroseconds(9620);
+  
+  // Print raw voltage value (number from 0 to 1023).
+  #ifdef PRINT_RAW_DATA
+  printValue("VoRaw", VoRaw, true);
+  Serial.println("");
+  #endif // PRINT_RAW_DATA
+  
+  // Use averaging if needed.
+  float Vo = VoRaw;
+  #ifdef USE_AVG
+  VoRawTotal += VoRaw;
+  VoRawCount++;
+  if ( VoRawCount >= N ) {
+    Vo = 1.0 * VoRawTotal / N;
+    VoRawCount = 0;
+    VoRawTotal = 0;
+  } else {
+    return;
+  }
+  #endif // USE_AVG
+
+  // Compute the output voltage in Volts.
+  Vo = Vo / 1024.0 * 5.0;
+  //printFValue("Vo", Vo*1000.0, "mV");
+
+  // Convert to Dust Density in units of ug/m3.
+  float dV = Vo - Voc;
+  if ( dV < 0 ) {
+    dV = 0;
+    Voc = Vo;
+  }
+  float dustDensity = dV / K * 100.0;
+
+    
+    //setting the sensor values as bytes of a single large payload which we will be sending via Lora to the Gatway.
+    uint32_t conc = int(dustDensity);
+    payload[0] = highByte(co2);
+    payload[1] = lowByte(co2);
+    payload[2] = highByte(tvoc);
+    payload[3] = lowByte(tvoc);
+    payload[4] = highByte(reading);
+    payload[5] = lowByte(reading);
+    payload[6] = highByte(conc);
+    payload[7] = lowByte(conc); 
+
+    os_runloop_once();
+}
